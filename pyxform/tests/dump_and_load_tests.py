71b96bdfe62afeca041682946104e9836dca9901
Alex Dorey <dorey415@gmail.com>
2011-06-19 21:13:08 -0400

    these tests pass using the new syntax.

--- pyxform/tests/dump_and_load_tests.py
+++ pyxform/tests/dump_and_load_tests.py
@@ -31,11 +31,11 @@ class DumpAndLoadTests(TestCase):
         for filename, survey in self.surveys.items():
             survey.json_dump()
             path = os.path.join(self.this_directory,
-                                survey.get_name() + ".json")
+                                survey.name + ".json")
             survey_from_dump = create_survey_from_path(path)
             self.assertEqual(survey.to_dict(), survey_from_dump.to_dict())
 
     def tearDown(self):
         for filename, survey in self.surveys.items():
-            path = survey.get_name() + ".json"
+            path = survey.name + ".json"
             os.remove(path)
